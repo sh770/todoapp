c380805b7e314593e16018d8688b89c0ba30a6aa
Shalom Harpaz <sh770h@gmail.com>
2023-12-20 20:48:10 +0200

    localStorage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let globalid = 0
-
-let todos = [];
+let globalid = 0;
+const saveTodos = JSON.parse(localStorage.getItem("todos"));
+let todos = saveTodos || [];
 
 const todoListElemrnt = document.getElementById("todo-list");
 const inputElemrnt = document.querySelector("input");
@@ -8,13 +8,10 @@ const inputElemrnt = document.querySelector("input");
 const myButton = document.getElementById("myButton");
 
 // הוסף אירוע לחיצה על הכפתור
-myButton.addEventListener("click", ()=> {
-    // קוד הפונקציה שתרצה להפעיל בעת לחיצה
-    createTodoItem();
+myButton.addEventListener("click", () => {
+  // קוד הפונקציה שתרצה להפעיל בעת לחיצה
+  createTodoItem();
 });
-
-
-
 
 const renderTodoList = () => {
   let todoComponents = "";
@@ -29,69 +26,57 @@ const createTodoComponent = (todo) => {
   const time = new Date(todo.created).toLocaleTimeString("he-il");
   return `
           <figure>
-            <h3 class="${todo.isCompleted ? "completed" : ""}">${todo.text}</h3>
-            <p class="${
-              todo.isCompleted ? "completed" : ""
-            }">${data} ${time}</p>
+            <h3 ${todo.isCompleted ? 'class="completed"' : ""}>${todo.text}</h3>
+            <p ${
+              todo.isCompleted ? 'class="completed"' : ""
+            }>${data} ${time}</p>
             <button onclick="toggleTodoItem(${todo.id})" 
-            class="toggle-todo ${todo.isCompleted ? 'completed' : ''}">${todo.isCompleted ? "בוצע" : "סמן"}</button>
-            <button onclick="removeTodoById(${todo.id})" class="remove-todo ">X</button>
+            class="toggle-todo ${todo.isCompleted ? "completed" : ""}">${
+    todo.isCompleted ? "בוצע" : "סמן"
+  }</button>
+            <button onclick="removeTodoById(${
+              todo.id
+            })" class="remove-todo ">X</button>
         </figure> 
     `;
 };
 
 const toggleTodoItem = (id) => {
-    const idx = todos.findIndex(todo => todo.id === id)
-    todos[idx].isCompleted = !todos[idx].isCompleted
-    renderTodoList()
-    // saveTodoListToJson()
+  const idx = todos.findIndex((todo) => todo.id === id);
+  todos[idx].isCompleted = !todos[idx].isCompleted;
+  saveTodoList();
+  renderTodoList();
 };
 
 const createTodoItem = () => {
-    const text = inputElemrnt.value
-    console.log(text);
-    if (!text) return
-    const todoItem = {
-        id:genId(),
-         text, 
-         created:Date.now(), 
-         isCompleted:false
-    }
-    todos.push(todoItem)
-    inputElemrnt.value = ""
-    renderTodoList()
-    // saveTodoListToJson()
-}
+  const text = inputElemrnt.value;
+  console.log(text);
+  if (!text) return;
+  const todoItem = {
+    id: genId(),
+    text,
+    created: Date.now(),
+    isCompleted: false,
+  };
+  todos.push(todoItem);
+  inputElemrnt.value = "";
+  saveTodoList();
+  renderTodoList();
+};
 
 const removeTodoById = (id) => {
-    todos = [...todos.filter(todo => todo.id !== id)]
-    renderTodoList()
-    // saveTodoListToJson()
-}
+  todos = [...todos.filter((todo) => todo.id !== id)];
+  saveTodoList();
+  renderTodoList();
+};
 
 const genId = () => {
-    globalid++
-    return globalid
-}
+  globalid++;
+  return globalid;
+};
 
-// renderTodoList();
+const saveTodoList = () => {
+  localStorage.setItem("todos",JSON.stringify(todos))
+};
 
-// const saveTodoListToJson = () => {
-//     const jsonContent = JSON.stringify(todos);
-//     localStorage.setItem("todoList", jsonContent);
-//   };
-  
-//   // פונקציה לטעינת רשימת הטודו מקובץ JSON
-//   const loadTodoListFromJson = () => {
-//     // כאן יש להשתמש באותה שיטה שבה שמרת את רשימת הטודו
-//     const jsonContent = localStorage.getItem("todoList");
-//     if (jsonContent) {
-//       todos = JSON.parse(jsonContent);
-//       renderTodoList(); // עדכון של התצוגה לאחר טעינה
-//     }
-//   };
-  
-
-  
-
-  
+renderTodoList()
